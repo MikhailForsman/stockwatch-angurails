e433538c7088da1ef0af57a196de0b8c96bca2d2
mikhail.salud@gmail.com <mikhail.salud@gmail.com>
2014-03-25 19:01:31 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stockwatch::Application.config.secret_key_base = 'dd847698961c99d71a41f28556b0e8b0ac48723e47b082a82b4be2157f3d808f566c8804185c83f5dc75fd4fbedbde63bb3291fc2033250b7f1b0bd5474eecb0'
